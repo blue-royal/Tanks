3ca8e944d6aab73acaadd5b8e0622776b3fe4936
Toby M <johndoe@example.com>
2022-05-16 10:50:36 +0100

    setup gameobject and tank class

--- src/sprites.py
+++ src/sprites.py
@@ -1,1 +1,14 @@
 from settings import *
+
+class GameObject():
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+    def draw():
+        print("Override default draw function")
+
+class Tank(GameObject):
+    def __init__(self, x, y, colour):
+        GameObject.__init__(self, x, y)
+        self.colour = colour
+    def draw():
+        pass
