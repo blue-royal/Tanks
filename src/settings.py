a7ed44969eb1ee5fbe6298c6ade12697484435f1
Toby M <johndoe@example.com>
2022-05-16 10:57:56 +0100

    Added important constants

--- src/settings.py
+++ src/settings.py
@@ -1,1 +1,15 @@
 import pygame as pg
+
+FPS = 30
+WIDTH = 600
+HEIGHT = 400
+
+#Testing colours
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+
+pg.display.set_mode((WIDTH, HEIGHT))
