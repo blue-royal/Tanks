a49338d0de54a3a0df454941b3091af229d42570
Toby M <johndoe@example.com>
2022-05-17 19:48:08 +0100

    click escape to leave program

--- src/main.py
+++ src/main.py
@@ -1,5 +1,36 @@
+import pygame as pg
 from sprites import *
 from ui import *
+from sys import exit
 
 pg.init()
+pg.display.set_caption("Tanks")
 clock = pg.time.Clock()
+
+test = PlayerTank(200, 200, BLUE)
+
+running = True
+while running:
+    clock.tick(FPS) 
+    
+    #gather events   
+    events = pg.event.get()
+    # check for the X button being pressed
+    for event in events:        
+        if event.type == pg.QUIT:
+            running = False
+        elif event.type == pg.KEYDOWN:
+            if event.key == pg.K_ESCAPE:
+                running = False
+    
+    # update sprites
+    test.update()
+    # render / draw sprites in correct order
+    screen.fill(WHITE)
+    
+    test.draw()
+    
+    pg.display.flip()       
+
+pg.quit()
+exit()
