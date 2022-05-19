b68ab86890adcae0f0b56e2a921629d4e119b4d9
Toby M <johndoe@example.com>
2022-05-19 10:28:01 +0100

    to do: add draw test function

--- src/ui.py
+++ src/ui.py
@@ -1,5 +1,14 @@
 from settings import *
 
+def drawText(x, y, w, h, message):
+    pass
+
 class Button():
     def __init__(self, x, y):
         self.x, self.y = x, y
+
+class MenuScreen():
+    def __init__(self):
+        pass
+    def update(self):
+        pass
